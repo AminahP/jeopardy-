9dd3b319a1f30dc923f755cae64fe6da3a0976a6
Aminahp <aypitt7@gmail.com>
2022-10-11 19:28:35 -0400

    j

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import GameBody from "./components/GameBody.js";
+import Header from "./components/Header.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      score: 0,
+      searchURL: "https://jservice.io/api/random"
+    };
+    this.updateScore = this.updateScore.bind(this);
+    this.getQuestion = this.getQuestion.bind(this);
+  }
+  async getData() {
+    try {
+      const data = await fetch(this.state.searchURL);
+      const JSONData = await data.json();
+      this.setState({
+        data: JSONData
+      });
+    } catch {
+      console.log("Failed to fetch data");
+    }
+  }
+  componentDidMount() {
+    this.getData();
+  }
+  getQuestion() {
+    this.getData();
+  }
+  updateScore(action) {
+    switch (action) {
+      case "add":
+        this.setState({ score: this.state.score + 1 });
+        break;
+      case "subtract":
+        this.setState({ score: this.state.score - 1 });
+        break;
+      case "reset":
+        this.setState({ score: 0 });
+        break;
+      default:
+        this.setState({ score: 0 });
+    }
+  }
+  render() {
+    if (this.state.data === null) {
+      return (
+        <div>
+          <p>loading data...</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Header />
+          <GameBody
+            data={this.state.data[0]}
+            updateScore={this.updateScore}
+            currentScore={this.state.score}
+            getQuestion={this.getQuestion}
+          />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
+
+
